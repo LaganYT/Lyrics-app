81c1267b1ec5ff03a2b7125863d9521596593583
Logan Latham <95306463+LaganYT@users.noreply.github.com>
2022-08-05 08:53:57 -0700

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import react from "react";
 export default function navbar(){
     return(
         <nav className="navbar navbar-dark bg-black mb-5">
-            <span className="navbar-brand mb-0 h1 mx-auto">LyricsFinder</span>
+            <span className="navbar-brand mb-0 h1 mx-auto" onclick="location.href='https://ltunes.gq'">Lyric Lookup</span>
         </nav>
     );
 };
